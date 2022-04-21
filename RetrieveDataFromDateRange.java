9971fdd6cb4605b28f06954f04bf08ab4f2304c5
barbadeadesh <barbadeadesh2020@email.com>
2022-04-21 11:41:35 +0530

    ADDED: Functionality for Retrieve Data From Date Range

---
+++ RetrieveDataFromDateRange.java
@@ -0,0 +1,61 @@
+package com.bridgelab.payroll_service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/*@purpose : Create class for performing different function in payroll service related with database
+ * 
+ * @author : Adesh Barbade
+ * 
+ * @since :
+ * 
+ */
+
+public class EmployeePayrollService {
+
+	public enum IOService{CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	private static EmployeePayrollDBService employeePayrollDBService;
+
+    public EmployeePayrollService() {
+        employeePayrollDBService = EmployeePayrollDBService.getInstance();
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList){
+        this();
+        this.employeePayrollList = employeePayrollList;
+    }
+
+	public List<EmployeePayrollData> readEmployeePayrollData(IOService ioservice) {
+		if (ioservice.equals(IOService.DB_IO)){
+			this.employeePayrollList= employeePayrollDBService.readData();
+		}
+		return employeePayrollList;
+	}
+
+	public void updateEmployeeSalary(String name, double salary) {
+		int result = employeePayrollDBService.updateEmployeeData(name, salary);
+		if (result == 0) return;
+		EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+		if (employeePayrollData != null) employeePayrollData.salary = salary;
+
+	}
+
+	private EmployeePayrollData getEmployeePayrollData(String name) {
+		return this.employeePayrollList.stream()
+				.filter(employeePayrollData -> employeePayrollData.name.equals(name))
+				.findFirst()
+				.orElse(null);
+	}
+
+	public boolean checkEmployeeInSyncWithDB(String name) {
+		List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+		return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
+	}
+	
+	public List<EmployeePayrollData> getEmployeePayrollDataByGivenDataRange(LocalDate startDate, LocalDate endDate) {
+        return employeePayrollDBService.getEmployeePayrollDataByDataRange(startDate,endDate);
+    }
+
+}
